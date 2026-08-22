df5b3d1119cfd6c2b614624ea521d7daf904834f
Rinsford <rinsfordb@gmail.com>
2026-08-22 00:20:57 +0000

    adding the tying animation

---
+++ script.js
@@ -0,0 +1,15 @@
+<script>
+  const text = "Hello, I am Rinsford";
+  const target = document.getElementById("typed-text");
+  let index = 0;
+
+  function typeText() {
+    if (index < text.length) {
+      target.textContent += text.charAt(index);
+      index++;
+      setTimeout(typeText, 100);
+    }
+  }
+
+  typeText();
+</script>
