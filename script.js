54af03a43f6678bfe760b1795388bce6c8cdf7f7
Rinsford <rinsfordb@gmail.com>
2026-08-24 20:06:43 +0000

    fixed sytnax allowing the website to run

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-<script>
   const text = "Hello, I am Rinsford";
   const target = document.getElementById("typed-text");
   let index = 0;
 
   function typeText() {
-    if (index < text.length) {
+    if (target && index < text.length) {
       target.textContent += text.charAt(index);
       index++;
       setTimeout(typeText, 100);
@@ -12,4 +11,3 @@
   }
 
   typeText();
-</script>
