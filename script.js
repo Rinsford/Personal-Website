80f2df01ce27d43688c61782f42cd06ae984c866
Rinsford <rinsfordb@gmail.com>
2026-09-04 15:37:12 +0000

    Fixed the Popup window

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const terminalInput = document.querySelector('.terminal-input');
 const output = document.querySelector('.terminal-output');
+const popupWindow = document.querySelector('#popup-window');
+const closePopup = document.querySelector('#close-popup');
 
 //Add the files and directories to be displayed when the 'ls' command is executed
 const filesAndDirectories = ['about-me', 'projects', 'contact'];
@@ -20,26 +22,24 @@ terminalInput.addEventListener('keydown', function(event) {
         //The terminal input clears after the command is executed
         terminalInput.value = '';
 
-        //If the user types the 'cd' command with the files name
-        //The hover window will appear with the information in that file
-        if (command === 'cd') {
-            
-            //If the user types 'cd about-me', the hover window will appear with the information in that file
-            if (command === 'cd about-me') {
-                const popupWindow = document.querySelector('.popup-window');
-                popupWindow.classList.remove('hidden');
-            }
-
-            //If the user types 'cd projects', the hover window will appear with the information in that file
-            if (command === 'cd projects') {
-                window.location.href = 'projects.html';
-            }
-
-            //If the user types 'cd contact', the hover window will appear with the information in that file
-            if (command === 'cd contact') {
-                window.location.href = 'contact.html';
-            }
-
+        // If the user types 'cd about-me', show the About Me popup.
+        if (command === 'cd about-me') {
+            popupWindow.classList.remove('hidden');
         }
+
+        // If the user types 'cd projects', open the projects popup.
+        if (command === 'cd projects') {
+            window.location.href = 'projects.html';
+        }
+
+        // If the user types 'cd contact', open the contact popup.
+        if (command === 'cd contact') {
+            window.location.href = 'contact.html';
+        }
+
     }
-})
+});
+
+closePopup.addEventListener('click', function() {
+    popupWindow.classList.add('hidden');
+});
