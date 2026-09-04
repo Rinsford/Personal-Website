34acaed89f9509cbd3919696522732bddb9c2203
Rinsford <rinsfordb@gmail.com>
2026-09-04 14:04:47 +0000

    Added the ability to enter ls and get a list in return

---
+++ script.js
@@ -0,0 +1,20 @@
+const terminalInput = document.querySelector('.terminal-input');
+const output = document.querySelector('.terminal-output');
+
+//Add the files and directories to be displayed when the 'ls' command is executed
+const filesAndDirectories = ['about-me', 'projects', 'contact'];
+
+
+terminalInput.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+         // Prevent form submission
+        event.preventDefault();
+
+        // Get the command entered by the user
+        const command = terminalInput.value.trim();
+
+        if (command === 'ls') {
+            output.textContent = filesAndDirectories.join('\n');
+        }
+    }
+})
